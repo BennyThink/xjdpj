fa8312b39c51fc99f501c72adae5d8c924067287
Benny <benny.think@gmail.com>
2025-04-16 20:14:24 +0200

    update scripts

--- backend/upgrade.ps1
+++ backend/upgrade.ps1
@@ -1,17 +1,21 @@
-Write-Host "Updating code"
-git pull
+# Move up one directory
 Set-Location ..
 
+Write-Host "Updating code"
+git pull
+
 Write-Host "Installing backend deps"
+Set-Location backend
 pdm install
+Set-Location ..
 
 Write-Host "Installing frontend deps"
 Set-Location frontend
 npm install
 npm run build
+Set-Location ..
 
 Write-Host "Prepare to run"
-Set-Location ..
 Set-Location backend
 $env:KEY = "123456"
 python main.py
--- backend/upgrade.sh
+++ backend/upgrade.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+cd ..
 
 echo "Updating code"
 git pull
-cd ..
 
 echo "Installing backend deps"
+cd backend
 pdm install
+cd ..
 
 echo "Installing frontend deps"
 cd frontend
 npm install
 npm run build
+cd ..
 
 echo "Prepare to run"
-cd ../backend
+cd backend
 export KEY="123456"
 python main.py
